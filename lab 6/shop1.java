d992135b1aa7331e796ceec0b73a7d64bb8c6b7b
Md. Sakib Hossaine <116342600+Sakib-Hossaine@users.noreply.github.com>
2024-03-03 17:53:57 +0600

    Add files via upload

---
+++ lab 6/shop1.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+
+public class Shop1 {
+    private String name;
+    private ArrayList<Item> items;
+
+    public Shop(String name) {
+        this.name = name;
+        this.items = new ArrayList<>();
+    }
+
+    private Item findItem(String id) {
+        for (Item item : items) {
+            if (item.getId().equals(id)) 
+            {
+                return item;
+            }
+        }
+        return null;
+    }
+
+    public void addItem(String name, String id, String category, double price, int count) {
+        Item item = findItem(id);
+        if (item != null) {
+            item.quantity += count;
+        } else {
+            items.add(new Item(name, id, category, price, count));
+        }
+    }
+
+    public void viewItem(String id) {
+        Item item = findItem(id);
+        if (item != null) {
+            System.out.println(item);
+        }
+    }
+
+    public void viewItems() {
+        for (Item item : items) {
+            System.out.println(item);
+        }
+    }
+
+    public void sellAnItem(String id, int quantity) {
+        Item item = findItem(id);
+        if (item != null) {
+            item.quantity -= quantity;
+        } else {
+            System.out.println("Item not found");
+        }
+    }
+
+    public double getSalePriceOfAnItem(String id, double saleAmount) {
+        Item item = findItem(id);
+        if (item != null) {
+            return item.getSalePrice(saleAmount);
+        } else {
+            System.out.println("Item not found");
+            return 0;
+        }
+    }
+}
